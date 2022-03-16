6d06b92d531cd238052c1ac81bc3631420ed80bc
avaatkins <99356366+avaatkins@users.noreply.github.com>
2022-03-16 10:06:59 -0700

    Create ChallengeH.rb

---
+++ ChallengeH.rb
@@ -0,0 +1,175 @@
+use_bpm 120
+use_synth :chiplead
+
+define :any_two_notes do |n1,n2|
+  play n1
+  play n2
+  sleep 0.5
+end
+
+# MEASURE 1 ---------------------------
+any_two_notes :a4, :a5
+#play :a4
+#play :a5
+#sleep 0.5
+
+any_two_notes :a4, :a5
+#play :a4
+#play :a5
+#sleep 0.5
+
+any_two_notes :c5, :c6
+#play :c5
+#play :c6
+#sleep 0.5
+
+any_two_notes :a4, :a5
+#play :a4
+#play :a5
+#sleep 0.5
+
+any_two_notes :d5, :d6
+#play :d5
+#play :d6
+#sleep 0.5
+
+any_two_notes :a4, :a5
+#play :a4
+#play :a5
+#sleep 0.5
+
+any_two_notes :e5, :e6
+#play :e5
+#play :e6
+#sleep 0.5
+
+any_two_notes :d5, :d6
+#play :d5
+#play :d6
+#sleep 0.5
+
+
+# MEASURE 2 ---------------------------
+any_two_notes :c5, :c6
+#play :c5
+#play :c6
+#sleep 0.5
+
+any_two_notes :c5, :c6
+#play :c5
+#play :c6
+#sleep 0.5
+
+any_two_notes :e5, :e6
+#play :e5
+#play :e6
+#sleep 0.5
+
+any_two_notes :c5, :c6
+#play :c5
+#play :c6
+#sleep 0.5
+
+any_two_notes :g5, :g6
+#play :g5
+#play :g6
+#sleep 0.5
+
+any_two_notes :c4, :c5
+#play :c4
+#play :c5
+#sleep 0.5
+
+any_two_notes :e5, :e6
+#play :e5
+#play :e6
+#sleep 0.5
+
+any_two_notes :c4, :c5
+#play :c4
+#play :c5
+#sleep 0.5
+
+
+# MEASURE 3 ---------------------------
+any_two_notes :g4, :g5
+#play :g4
+#play :g5
+#sleep 0.5
+
+any_two_notes :g4, :g5
+#play :g4
+#play :g5
+#sleep 0.5
+
+any_two_notes :b4, :b5
+#play :b4
+#play :b5
+#sleep 0.5
+
+any_two_notes :g4, :g5
+#play :g4
+#play :g5
+#sleep 0.5
+
+any_two_notes :c5, :c6
+#play :c5
+#play :c6
+#sleep 0.5
+
+any_two_notes :g4, :g5
+#play :g4
+#play :g5
+#sleep 0.5
+
+any_two_notes :d5, :d6
+#play :d5
+#play :d6
+#sleep 0.5
+
+any_two_notes :c5, :c6
+#play :c5
+#play :c6
+#sleep 0.5
+
+
+# MEASURE 4 ---------------------------
+any_two_notes :f4, :f5
+#play :f4
+#play :f5
+#sleep 0.5
+
+any_two_notes :f4, :f5
+#play :f4
+#play :f5
+#sleep 0.5
+
+any_two_notes :a4, :a5
+#play :a4
+#play :a5
+#sleep 0.5
+
+any_two_notes :f4, :f5
+#play :f4
+#play :f5
+#sleep 0.5
+
+any_two_notes :c5, :c6
+#play :c5
+#play :c6
+#sleep 0.5
+
+any_two_notes :f4, :f5
+#play :f4
+#play :f5
+#sleep 0.5
+
+any_two_notes :c5, :c6
+#play :c5
+#play :c6
+#sleep 0.5
+
+any_two_notes :b4, :b5
+#play :b4
+#play :b5
+#sleep 0.5
