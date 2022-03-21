086a767e9ab2bb744e6dce09b6ba3fe2fe664901
avaatkins <99356366+avaatkins@users.noreply.github.com>
2022-03-21 10:52:30 -0700

    Create challengeeC.rb

---
+++ challengeeC.rb
@@ -0,0 +1,75 @@
+# Second set of eigth notes
+play :c3
+sleep 0.5
+play :b2
+sleep 0.5
+play :g2
+sleep 0.5
+play :e2
+sleep 0.5
+
+with_fx :bitcrusher do
+end
+
+with_fx :ixi_techno do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  
+  # Second set of eigth notes
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+
+with_fx :whammy do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  
+  # Second set of eigth notes
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+
+with_fx :slicer do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  
+  # Second set of eigth notes
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
